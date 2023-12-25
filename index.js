d4f8e71a94d1978b8f07ba9d53dcb68a2fa8cbd9
Mabu1 <umahtab65@gmail.com>
2023-12-25 11:59:01 +0600

    final

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const port = process.env.PORT || 5001;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4xqxhsy.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
@@ -35,34 +35,53 @@ async function run() {
         const userCollection = client.db("userDB").collection("users");
         const announcementCollection = client.db("userDB").collection("announcement");
         const cuponsCollection = client.db("userDB").collection("cupons");
-        
+
         app.get("/apartment", async (req, res) => {
-            const result = await appartmentCollection.find().toArray();
-            console.log(result);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log('Pagination query', req.query)
+            const result = await appartmentCollection.find({ buyOption: true })
+                .skip(page * size)
+                .limit(size)
+                .toArray();
+                
             res.send(result);
+
+        });
+
+        app.get("/apartmentCount", async (req, res) => {
+            try {
+              const count = await appartmentCollection.countDocuments({ buyOption: true });
+              console.log(count);
+              res.send({ count });
+            } catch (error) {
+              console.error("Error fetching count:", error);
+              res.status(500).send({ error: "Internal Server Error" });
+            }
           });
-      
-      
-          app.put('/apartment/:id', async (req, res) => {
+          
+          
+
+        app.put('/apartment/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const options = { upsert: true };
             const updatedApartment = req.body;
-          
+
             const apartment = {
                 $set: {
-          
+
                     buyOption: updatedApartment.buyOption,
-                   
-                   
-                   
+
+
+
                 }
             }
-          
+
             const result = await appartmentCollection.updateOne(filter, apartment, options);
             res.send(result);
-          })
-         
+        })
+
 
 
         //   users
@@ -71,8 +90,8 @@ async function run() {
             const result = await userCollection.find().toArray();
             console.log(result);
             res.send(result);
-          });
-      
+        });
+
 
         app.post("/users", async (req, res) => {
             try {
@@ -81,7 +100,7 @@ async function run() {
                 console.log(result);
                 res.send(result);
 
-                
+
             } catch (error) {
                 console.log(error);
 
@@ -89,26 +108,26 @@ async function run() {
 
         });
 
-   
+
         app.put('/users/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const options = { upsert: true };
             const updatedUser = req.body;
-          
+
             const use = {
                 $set: {
-          
+
                     status: updatedUser.status,
-                   
-                   
-                   
+
+
+
                 }
             }
-          
+
             const result = await userCollection.updateOne(filter, use, options);
             res.send(result);
-          })
+        })
 
 
         //   announcement
@@ -116,16 +135,16 @@ async function run() {
             const result = await announcementCollection.find().toArray();
             console.log(result);
             res.send(result);
-          });
-      
-          app.post("/announcement", async (req, res) => {
+        });
+
+        app.post("/announcement", async (req, res) => {
             try {
                 const body = req.body;
                 const result = await announcementCollection.insertOne(body);
                 console.log(result);
                 res.send(result);
 
-                
+
             } catch (error) {
                 console.log(error);
 
@@ -133,27 +152,27 @@ async function run() {
 
         });
 
-    //cupons
-    app.get("/cupons", async (req, res) => {
-        const result = await cuponsCollection.find().toArray();
-        console.log(result);
-        res.send(result);
-      });
-
-      app.post("/cupons", async (req, res) => {
-        try {
-            const body = req.body;
-            const result = await cuponsCollection.insertOne(body);
+        //cupons
+        app.get("/cupons", async (req, res) => {
+            const result = await cuponsCollection.find().toArray();
             console.log(result);
             res.send(result);
+        });
 
-            
-        } catch (error) {
-            console.log(error);
+        app.post("/cupons", async (req, res) => {
+            try {
+                const body = req.body;
+                const result = await cuponsCollection.insertOne(body);
+                console.log(result);
+                res.send(result);
 
-        }
 
-    });
+            } catch (error) {
+                console.log(error);
+
+            }
+
+        });
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!!!");
